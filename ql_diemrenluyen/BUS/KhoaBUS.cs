1f1b1bc06cca6684194065a7188690199ff9c841
Bonniee <nguyenxuantruc.1203@gmail.com>
2024-10-24 15:26:37 +0700

    update

---
+++ ql_diemrenluyen/BUS/KhoaBUS.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ql_diemrenluyen.DAO;
+using ql_diemrenluyen.DTO;
+
+namespace ql_diemrenluyen.BUS
+{
+    internal class KhoaBUS
+    {
+        public static KhoaDTO GetKhoaByID(long id)
+        {
+            return KhoaDAO.GetKhoaByID(id);
+        }
+    }
+}
---
+++ ql_diemrenluyen/BUS/LopBUS.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ql_diemrenluyen.DAO;
+using ql_diemrenluyen.DTO;
+
+namespace ql_diemrenluyen.BUS
+{
+    class LopBUS
+    {
+        //Lấy Đối tượng lớp từ id của lớp
+        public static LopDTO GetLopByID(long idLop)
+        {
+            return LopDAO.GetLopByID(idLop);
+        }
+    }
+}
